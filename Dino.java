010e376650ef1339020cc394da367cccbec13d7d
Natalie Ramirez <60053094+natalieramirez@users.noreply.github.com>
2020-12-12 15:31:41 -0500

    update dino

--- Dino.java
+++ Dino.java
@@ -1,12 +1,30 @@
+// Hack TJ main DinoGame class
+// Natalie and Sarah
 
-import javax.swing.ImageIcon;
+import javax.swing.*;
+import java.awt.Color;
 
-public class Dino {
+public class DinoGame extends JFrame
+{
+    public static final int frameX = 800;
+    public static final int frameY = 400;
 
-    public statice ImageIcon dino = new ImageIcon("dino.png");
-    public static void main(String[] args) {
-
-
+    public static DinoGame window;
     
+    public DinoGame()
+    {
+        setBounds(0, 0, frameX, frameY);
+        setTitle("dino xmas game");
+        setBackground(Color.WHITE);
+        setResizable(false);
+        setAlwaysOnTop(true);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) 
+    {
+        window = new DinoGame();
+        window.setVisible(true);
     }
 }
--- Graphics.java
+++ Graphics.java
@@ -2,15 +2,6 @@
 natalie - graphics
 12/12/20  */
 
-
-import javax.swing.JPanel;
-import java.awt.Color;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.awt.Font;
-import javax.swing.ImageIcon;
-
-public class Graphics extends JPanel {
+public class Graphics {
     
 }
