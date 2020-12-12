995077f81e2d22b1ce1a2c68d78e47300bd2504d
sj-fleming <sarah.fleming22@bishopireton.org>
2020-12-12 15:14:52 -0500

    create dino.java

---
+++ Dino.java
@@ -0,0 +1,12 @@
+
+import javax.swing.ImageIcon;
+
+public class Dino {
+
+    public statice ImageIcon dino = new ImageIcon("dino.png");
+    public static void main(String[] args) {
+
+
+    
+    }
+}
