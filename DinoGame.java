ac055de77fbdfb6ed322e5b81455a310d24129d3
Natalie Ramirez <60053094+natalieramirez@users.noreply.github.com>
2020-12-12 16:15:06 -0500

    created frame

--- DinoGame.java
+++ DinoGame.java
@@ -3,8 +3,10 @@
 
 import javax.swing.*;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
 
-public class DinoGame extends JFrame
+public class DinoGame extends JFrame 
 {
     public static final int frameX = 800;
     public static final int frameY = 400;
@@ -13,18 +15,25 @@ public class DinoGame extends JFrame
     
     public DinoGame()
     {
-        setBounds(0, 0, frameX, frameY);
+        setSize(frameX, frameY);
         setTitle("dino xmas game");
         setBackground(Color.WHITE);
-        setResizable(false);
+        //setResizable(false);
         setAlwaysOnTop(true);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    @Override public void paint(Graphics g)
+    {
+        Dimension d = this.getSize();
+        g.setColor(Color.lightGray);
+        g.drawLine(0,d.height/2 + 100,d.width,d.height/2+100);
     }
 
     public static void main(String[] args) 
     {
         window = new DinoGame();
         window.setVisible(true);
+        window.setLocationRelativeTo(null);
     }
 }
