34a117344503c1495e3102b48e763a5468a7fd97
sj-fleming <sarah.fleming22@bishopireton.org>
2020-12-12 23:09:12 -0500

    created tree array in trees.java

--- Trees.java
+++ Trees.java
@@ -1,18 +1,47 @@
 /* HackTJ Dino Game
  Method to move trees across the background */
  
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
+
 public class Trees extends DinoGame{
  
-   Trees tree1 = new Trees();
    private int xPos, yPos;
+   private ImageIcon[] trees = new ImageIcon[4];
+
+   Trees(int x) {
+      xPos = x;
+      yPos = 180;
+      for(int i = 0; i < trees.length; i++)
+         trees[i] = new ImageIcon("imgs/trees.jpg");
+   }
    
+   public void draw (Graphics g) {
+      for(int i = 0; i < trees.length; i++)
+         g.drawImage(trees[i].getImage(), 200, 100, null);
+  }
+
+/*  private void createJumpTimer() {
+   Timer treeTimer = new Timer (250,
+  
+  /* new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent evt) {
+         dino.jumpDown();
+         repaint();
+         jumpTimer.stop();
+      }
+   }
+   
+}
+  */ 
    public void scroll (){
       while (gameRunning == true) {
          xPos--;
          if (xPos <0)
             xPos = 800;
       }
-      
       
    }
  
